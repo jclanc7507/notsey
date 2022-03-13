f7dc433e7cb60aec0cf79c383eb6d511b95f72ac
Jeremiah <jclanc7507@gmail.com>
2022-03-13 10:57:47 -0400

    finished writing note taking routes, working on testing and pushing notes into json

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,3 +1,33 @@
 const router = require("express").Router();
-const { createNewNote, validateNote } = require('../../lib/notes');
+const { createNewNote, validateNote, findById } = require('../../lib/notes');
 const { notes } = require('../../db/notes');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if(req.id) {
+        results = findById(req.id, results);
+    }
+    res.json(results);
+});
+
+router.get('/animals/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(results);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
